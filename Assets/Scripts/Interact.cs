83945dc72e2bf337f5754fdf8a2094621014b170
Lady Lexxie <u2061581@unimail.hud.ac.uk>
2022-01-10 16:23:40 +0000

    Object Pickup

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -9,7 +9,9 @@ public class Interact : MonoBehaviour {
         Ray ray = GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
         
         if (Physics.Raycast(ray, out hit, raycastDistance)) {
-            if(hit.collider.tag == "Enemy"){
+            if(hit.collider.tag == "Interactible" && Input.GetButton("Jump")){
+                Transform objectHit = hit.transform;
+                objectHit.position = transform.position + ray.direction * raycastDistance;
                 //TODO - add something
             }
         }
