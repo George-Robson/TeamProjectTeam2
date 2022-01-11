b6da793c4f3d392c8ab0bf13499528ce30da81f8
George Robson <grobson97@gmail.com>
2022-01-11 11:45:41 +0000

    Audio Volume

--- Assets/Scripts/AudioVolume.cs
+++ Assets/Scripts/AudioVolume.cs
@@ -7,6 +7,7 @@ public class AudioVolume : MonoBehaviour
 {
     private AudioSource audioSource;
     public AudioClip audioClip;
+    [SerializeField, Range(0f, 1f)] private float volume = 1;
     void Start() {
         audioSource = GameObject.Find("Player").transform.GetChild(0).GetComponent<Camera>().GetComponent<AudioSource>();
     } 
@@ -19,7 +20,7 @@ public class AudioVolume : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player") && audioClip != null)
         {
-            audioSource.PlayOneShot(audioClip);
+            audioSource.PlayOneShot(audioClip, volume);
         } else Debug.LogError("No audio clip assigned to play");
     }
 }
