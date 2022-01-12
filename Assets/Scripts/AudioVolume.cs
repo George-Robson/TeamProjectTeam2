6d24c1954e402ce241868c79b150e7e2e3dc80d4
George Robson <grobson97@gmail.com>
2022-01-12 13:31:47 +0000

    Fonts and stuff

--- Assets/Scripts/AudioVolume.cs
+++ Assets/Scripts/AudioVolume.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class AudioVolume : MonoBehaviour
